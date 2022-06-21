b1f51c00371014bd585726a99fc515ca95746351
Juan Rosario <jrosario19@hotmail.com>
2022-06-21 17:00:00 -0400

    Add tests for pages

---
+++ src/tests/pages.test.js
@@ -0,0 +1,49 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import DetailsPage from '../pages/DetailsPage';
+import store from '../redux/configureStore';
+import HomePage from '../pages/HomePage';
+
+
+
+describe('Renders pages precisely', () => {
+  test('Renders Homepage  precisely', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <HomePage />
+        </Provider>,
+      )
+      .toJSON();
+    render(
+      <Provider store={store}>
+        <HomePage />
+      </Provider>,
+    );
+
+    const element = document.querySelector('.criptocoins-list');
+    expect(element).toBeTruthy();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Renders Detailspage  precisely', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <DetailsPage />
+        </Provider>,
+      )
+      .toJSON();
+    render(
+      <Provider store={store}>
+        <DetailsPage />
+      </Provider>,
+    );
+
+    const element = document.querySelector('.criptocoin-container-detail');
+    expect(element).toBeTruthy();
+    expect(tree).toMatchSnapshot();
+  });
+});
